d53a420c5da4350848ff011d672538fe4fa8bf81
ivangonzalez224 <ivan.gonzalezr224@gmail.com>
2023-09-19 06:21:38 -0500

    Update queries for the animals date of birth

--- queries.sql
+++ queries.sql
@@ -9,7 +9,7 @@ SELECT * FROM animals WHERE NAME LIKE '%_mon';
 -- List the name of all animals born between 2016 and 2019:
 
 SELECT name FROM animals WHERE date_of_birth >= '2016-01-01' 
- AND  date_of_birth <  '2019-01-01';
+ AND  date_of_birth <=  '2019-12-31';
 
 -----------------------------------------------------------
 
